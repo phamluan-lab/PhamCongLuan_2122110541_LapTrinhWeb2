2bb59e2279b547d2223f27435d2a3e0001a2ae56
phamluan-lab <luancong524@gmail.com>
2025-05-15 13:51:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    accessKey: 'F8BBA842ECF85',
+    secretKey: 'K951B6PE1waDMi640xX08PD3vg6EkVlz',
+    orderInfo: 'pay with MoMo',
+    partnerCode: 'MOMO',
+    redirectUrl: 'http://localhost:3000/order',
+    ipnUrl: 'https://0778-14-178-58-205.ngrok-free.app/callback',
+    requestType: 'payWithMethod',
+    extraData: '',
+    orderGroupId: '',
+    autoCapture: true,
+    lang: 'vi'
+  }; 
